930fdb1003171118373df03e296c31f8de253dbf
caglitos <138796031+caglitos@users.noreply.github.com>
2025-04-23 01:01:21 +0000

    Pending changes exported from your codespace

--- src/Pags/aboutMe.tsx
+++ src/Pags/aboutMe.tsx
@@ -11,11 +11,6 @@ function app() {
         <div className="barras5 barra"></div>
       </div>
       <main>
-        <img
-          src="https://www.chileclimbers.cl/wp-content/uploads/2019/09/final.jpg"
-          alt="Foto mia" // ¡Siempre incluye 'alt' para accesibilidad!
-          className="mi-ft" // Clase CSS opcional
-        />
         <div className="presentacion">
           <h1>Hola, soy Carlos</h1>
           <h2>Desarrollador Web full-stack</h2>
